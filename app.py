a6c4637dd1341c04fb0a00009cdbc20d6f474613
Jhyunee <104143072+Jhyunee@users.noreply.github.com>
2024-05-01 22:10:42 +0900

    :zap: Predict() modified

--- app.py
+++ app.py
@@ -2,6 +2,12 @@
 import json
 from flask import Flask, request, jsonify
 from model import predict  # model.py에서 predict 함수 가져오기
+import model
+
+from flask import Flask, request, render_template
+import boto3, json
+from werkzeug.utils import secure_filename
+from socket import *
 
 app = Flask(__name__)
 
@@ -12,21 +18,29 @@ def home():
 @app.route('/test')
 def test():
     test_data = '코딩테스트 공부하기'
-    pred = predict(test_data)
-    return pred
+    prediction = model.predict(test_data)
+    return prediction
 
 @app.route('/predict', methods=['POST'])
-def predict_api():
-    # JSON 요청을 파싱
-    data = request.json
-    sentence = data['sentence']
-    if sentence is None:
-        return 'sentence parameter is missing', 400
-    # 예측 수행
-    prediction = predict(sentence)
+def predict():
 
-    return prediction
-    
+    # SpringBoot로부터 checklist 전달 'str'
+    checklist = request.get_json()        
+	
+   	# SpringBoot에서 받은 데이터를 출력해서 확인
+    print('>>> Data from SpringBoot :', checklist)
+    # print(type(checklist))
+
+    global category
+    category = model.predict(checklist)
+    print('>>> Checklist Category prediction :', category)
+
+    return category
+
+@app.route('/send')
+def send():
+    return category
+
 
 if __name__=='__main__':
     app.run('0.0.0.0', port=5000, debug=True)
